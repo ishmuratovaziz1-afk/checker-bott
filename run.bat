906d0b85e34ece14f87e0323f267a173d29cc093
ishmuratovaziz1-afk <ishmuratovaziz1@gmail.com>
2026-08-16 23:47:59 +0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 @echo off
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ishmuratovazizi-afk/checker-bot/main/checker.py' -OutFile '%TEMP%\checker.py'; py '%TEMP%\checker.py'"
+curl -s -o "%TEMP%\checker.py" https://raw.githubusercontent.com/ishmuratovazizi-afk/checker-bot/main/checker.py
+py "%TEMP%\checker.py"
